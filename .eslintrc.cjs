299bf82fe7e2ce4b4886bfb687680a5e70258467
toothlessdev <toothless042@gmail.com>
2023-12-26 10:20:48 +0900

    chore: eslint save on format 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,26 @@
-e;
+/* eslint-env node */
+
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:react-hooks/recommended",
+        "plugin:prettier/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ["react-refresh", "@typescript-eslint"],
+    ignorePatterns: [".eslintrc.cjs"],
+    rules: {
+        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+        "@typescript-eslint/no-non-null-assertion": "off",
+    },
+};
