0c1bf8565f0230fbd8906adf2686742a8b43ccbd
toothlessdev <toothless042@gmail.com>
2023-12-27 03:13:01 +0900

    feat: Signin 라우트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import "@/styles/globals.scss";
 import { Route, Routes } from "react-router-dom";
-
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
 import { MainLayout } from "./layouts/MainLayout";
 
 import HomePage from "./pages/home-page/HomePage";
-
-import "@/styles/globals.scss";
+import SigninPage from "./pages/auth-page/SigninPage";
+import SignupPage from "./pages/auth-page/SignupPage";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
     return (
@@ -15,7 +16,12 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<MainLayout />}>
                     <Route path="/" element={<HomePage />} />
+
+                    <Route path="*" element={<PageNotFound />} />
                 </Route>
+
+                <Route path="/auth/signin" element={<SigninPage />} />
+                <Route path="/auth/signup" element={<SignupPage />} />
             </Routes>
         </Provider>
     );
---
+++ src/constants/SocialLogin.ts
@@ -0,0 +1,36 @@
+export interface ISocialLoginProvider {
+    icon: string;
+    text: string;
+}
+
+export const SocialLoginProviders: ISocialLoginProvider[] = [
+    {
+        icon: "./icons/icon-google.png",
+        text: "구글 계정으로 시작",
+    },
+    {
+        icon: "./icons/icon-naver.png",
+        text: "네이버 계정으로 시작",
+    },
+    {
+        icon: "./icons/icon-kakao.png",
+        text: "카카오 계정으로 시작",
+    },
+];
+
+/*
+export const NAVER_URL =
+    "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
+    import.meta.env.VITE_NAVER_CLIENT_ID +
+    "&state=" +
+    process.env.REACT_APP_NAVER_STATE +
+    "&redirect_uri=" +
+    process.env.REACT_APP_NAVER_REDIRECT_URI;
+
+export const KAKAO_URL =
+    "https://kauth.kakao.com/oauth/authorize?client_id=" +
+    process.env.REACT_APP_REST_API_KEY +
+    "&redirect_uri=" +
+    process.env.REACT_APP_REDIRECT_URI +
+    "&response_type=code";
+*/
