bbae89589ccbb13a2b9b77459eeb5f2f98a21bf1
toothlessdev <toothless042@gmail.com>
2023-12-26 13:23:52 +0900

    fix: NavAside interaction 버그 해결

--- src/interfaces/navigation/NavAside.tsx
+++ src/interfaces/navigation/NavAside.tsx
@@ -17,24 +17,31 @@ export interface INavAsideItem {
 export const NavAside = () => {
     const { isNavOpen } = useSelector((state: RootState) => state.UserInterface);
 
+    const navRef = useRef<HTMLDivElement>(null);
     const asideRef = useRef<HTMLDivElement>(null);
     const asideBgRef = useRef<HTMLDivElement>(null);
 
     useLayoutEffect(() => {
-        if (asideRef.current && asideBgRef.current) {
+        let timeout: ReturnType<typeof setTimeout> | null = null;
+        if (asideRef.current && asideBgRef.current && navRef.current) {
             if (isNavOpen) {
+                navRef.current.style.zIndex = `1`;
                 asideBgRef.current.style.display = `block`;
                 asideRef.current.style.transform = `translateX(0px)`;
             }
             if (!isNavOpen) {
                 asideBgRef.current.style.display = `none`;
                 asideRef.current.style.transform = `translateX(300px)`;
+                timeout = setTimeout(() => navRef.current && (navRef.current.style.zIndex = `0`), 500);
             }
         }
+        return () => {
+            if (timeout != null) clearTimeout(timeout);
+        };
     }, [isNavOpen]);
 
     return (
-        <div className={styles.nav_aside}>
+        <div ref={navRef} className={styles.nav_aside}>
             <div ref={asideBgRef} className={styles.nav_aside_background}></div>
             <div ref={asideRef} className={styles.nav_aside_wrapper}>
                 <aside className={styles.nav_aside_container}>
