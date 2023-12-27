cb1ba610f5b20683c18bb8fe1aa8352f82c560e3
toothlessdev <toothless042@gmail.com>
2023-12-28 04:20:45 +0900

    feat: InputButtonContainer 컴포넌트 구현

--- src/interfaces/forms/Input.tsx
+++ src/interfaces/forms/Input.tsx
@@ -1,13 +1,14 @@
-import { forwardRef } from "react";
+import { MouseEventHandler, forwardRef } from "react";
+import { Button } from "./Button";
 import styles from "./Input.module.scss";
 
 export interface IInput {
     type?: string;
     width?: string;
     height?: string;
-    disabled?: boolean;
     rest?: unknown;
     placeholder?: string;
+    onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 export interface IInputContainer extends IInput {
@@ -25,8 +26,14 @@ export interface ITextAreaContainer extends ITextArea {
     label: string;
 }
 
-export const Input = forwardRef<HTMLInputElement, IInput>(({ width, height, disabled, ...rest }, ref) => {
-    return <input ref={ref} className={styles.input} disabled={disabled} style={{ width: width, height: height }} {...rest} />;
+export interface IInputButtonContainer extends IInputContainer {
+    btnWidth: string;
+    btnLabel: string;
+    onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+export const Input = forwardRef<HTMLInputElement, IInput>(({ width, height, onChange, ...rest }, ref) => {
+    return <input ref={ref} className={styles.input} style={{ width: width, height: height }} onChange={onChange} {...rest} />;
 });
 
 export const InputContainer = forwardRef<HTMLInputElement, IInputContainer>(({ width, height, label, ...rest }, ref) => {
@@ -37,6 +44,22 @@ export const InputContainer = forwardRef<HTMLInputElement, IInputContainer>(({ w
         </div>
     );
 });
+
+export const InputButtonContainer = forwardRef<HTMLInputElement, IInputButtonContainer>(
+    ({ width, height, label, btnWidth, btnLabel, onClick, ...rest }, ref) => {
+        return (
+            <div className={styles.input_btn_wrapper} style={{ width: width }}>
+                <p>{label}</p>
+                <div className={styles.input_btn_container}>
+                    <Input ref={ref} width={`calc(${width as string} - ${btnWidth})`} height={height} {...rest} />
+                    <Button type="primary-stroke" width={btnWidth} onClick={onClick}>
+                        {btnLabel}
+                    </Button>
+                </div>
+            </div>
+        );
+    },
+);
 
 export const TextArea = forwardRef<HTMLTextAreaElement, ITextArea>(({ width, height, cols, rows }, ref) => {
     return <textarea ref={ref} className={styles.textarea} cols={cols} rows={rows} style={{ width: width, height: height }}></textarea>;
---
+++ src/interfaces/overlay/Modal.tsx
@@ -0,0 +1,5 @@
+import styles from "./Modal.module.scss";
+
+export const Modal = () => {
+    return <div className={styles.modal_wrapper}></div>;
+};
