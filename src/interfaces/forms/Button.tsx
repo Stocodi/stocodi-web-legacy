4d0a9e4ae0025e46aeaec0fd5e9ddbd8d46444cb
toothlessdev <toothless042@gmail.com>
2023-12-28 20:32:16 +0900

    feat: 강의 카드, 슬라이더 구현

--- src/interfaces/forms/Button.tsx
+++ src/interfaces/forms/Button.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { MouseEventHandler } from "react";
 import styles from "./Button.module.scss";
 
@@ -6,6 +8,12 @@ export interface IButton {
     width?: string;
     height?: string;
     children?: React.ReactNode;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface IButtonCircle {
+    type: "left" | "right";
+    className?: string;
     onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
@@ -34,4 +42,13 @@ export const Button: React.FC<IButton> = ({ type, width, height, children, onCli
             </button>
         );
     }
+};
+
+export const ButtonCircle: React.FC<IButtonCircle> = ({ type, className, onClick, ...rest }) => {
+    return (
+        <button className={`${styles.btn_circle} ${className ?? ""}`} onClick={onClick} {...rest}>
+            {type === "left" && <FontAwesomeIcon size="xl" icon={faChevronLeft} />}
+            {type === "right" && <FontAwesomeIcon size="xl" icon={faChevronRight} />}
+        </button>
+    );
 };
--- src/pages/lecture-page/LecturePage.tsx
+++ src/pages/lecture-page/LecturePage.tsx
@@ -4,6 +4,14 @@ import { useNavigate } from "react-router-dom";
 import { Search } from "../../interfaces/forms/Search";
 import { Badge } from "../../interfaces/display/Badge";
 
+import { LectureSection } from "../../components/lecture-page/LectureSection";
+import { LectureCard } from "../../components/lecture-page/LectureCard";
+
+import styles from "./LecturePage.module.scss";
+
+//test
+import { LectureData } from "../../constants/__test__/Lecture";
+
 export default function LecturePage() {
     const navigate = useNavigate();
 
@@ -17,9 +25,47 @@ export default function LecturePage() {
 
     return (
         <>
-            강의페이지 - 메인
-            <Search ref={searchRef} onClick={onSearchBtnClick}></Search>
-            <Badge type="primary-stroke">해시태그</Badge>
+            <div className={styles.search_section}>
+                <Search ref={searchRef} onClick={onSearchBtnClick}></Search>
+
+                <div className={styles.badge_container}>
+                    <span>인기태그👉</span>
+                    <Badge type="primary-stroke">해시태그</Badge>
+                    <Badge type="primary-stroke">해시태그</Badge>
+                    <Badge type="primary-stroke">해시태그</Badge>
+                    <Badge type="primary-stroke">해시태그</Badge>
+                    <Badge type="primary-stroke">해시태그</Badge>
+                    <Badge type="primary-stroke">해시태그</Badge>
+                </div>
+            </div>
+
+            <LectureSection title="지금 시청중인 강의">
+                {LectureData.map((element, index) => {
+                    return (
+                        <LectureCard.Default
+                            key={index}
+                            title={element.title}
+                            publisher={element.publisher}
+                            imgSrc={element.imgSrc}
+                            tags={element.tags}
+                        />
+                    );
+                })}
+            </LectureSection>
+
+            <LectureSection title="실시간 인기강의">
+                {LectureData.map((element, index) => {
+                    return (
+                        <LectureCard.Default
+                            key={index}
+                            title={element.title}
+                            publisher={element.publisher}
+                            imgSrc={element.imgSrc}
+                            tags={element.tags}
+                        />
+                    );
+                })}
+            </LectureSection>
         </>
     );
 }
