32b5b8bd16601ebafb8e5e5d515dba00932e20f8
toothlessdev <toothless042@gmail.com>
2023-12-28 21:30:38 +0900

    feat: CheckBox, TextArea 컴포넌트 구현

--- src/interfaces/forms/Input.tsx
+++ src/interfaces/forms/Input.tsx
@@ -5,6 +5,7 @@ export interface IInput {
     type?: string;
     width?: string;
     height?: string;
+    disabled?: boolean;
     rest?: unknown;
     placeholder?: string;
 }
@@ -13,8 +14,19 @@ export interface IInputContainer extends IInput {
     label: string;
 }
 
-export const Input = forwardRef<HTMLInputElement, IInput>(({ width, height, ...rest }, ref) => {
-    return <input ref={ref} className={styles.input} style={{ width: width, height: height }} {...rest} />;
+export interface ITextArea {
+    width?: string;
+    height?: string;
+    cols?: number;
+    rows?: number;
+}
+
+export interface ITextAreaContainer extends ITextArea {
+    label: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, IInput>(({ width, height, disabled, ...rest }, ref) => {
+    return <input ref={ref} className={styles.input} disabled={disabled} style={{ width: width, height: height }} {...rest} />;
 });
 
 export const InputContainer = forwardRef<HTMLInputElement, IInputContainer>(({ width, height, label, ...rest }, ref) => {
@@ -22,6 +34,19 @@ export const InputContainer = forwardRef<HTMLInputElement, IInputContainer>(({ w
         <div className={styles.input_container} style={{ width: width }}>
             <p>{label}</p>
             <Input ref={ref} width="inherit" height={height} {...rest} />
+        </div>
+    );
+});
+
+export const TextArea = forwardRef<HTMLTextAreaElement, ITextArea>(({ width, height, cols, rows }, ref) => {
+    return <textarea ref={ref} className={styles.textarea} cols={cols} rows={rows} style={{ width: width, height: height }}></textarea>;
+});
+
+export const TextAreaContainer = forwardRef<HTMLTextAreaElement, ITextAreaContainer>(({ width, height, cols, rows, label }, ref) => {
+    return (
+        <div className={styles.input_container} style={{ width: width }}>
+            <p>{label}</p>
+            <TextArea ref={ref} width="inherit" height={height} rows={rows} cols={cols} />
         </div>
     );
 });
