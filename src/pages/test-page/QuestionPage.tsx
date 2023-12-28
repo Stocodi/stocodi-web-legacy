b72635d128bab303489b7ed807c293582d27dca4
toothlessdev <toothless042@gmail.com>
2023-12-29 01:36:34 +0900

    feat: 금융역량테스트 페이지 스켈레톤 코드

---
+++ src/pages/test-page/QuestionPage.tsx
@@ -0,0 +1,3 @@
+export default function QuestionPage() {
+    return <>문항 페이지</>;
+}
---
+++ src/pages/test-page/TestPage.tsx
@@ -0,0 +1,5 @@
+import styles from "./TestPage.module.scss";
+
+export default function TestPage() {
+    return <>금융 역량 테스트</>;
+}
