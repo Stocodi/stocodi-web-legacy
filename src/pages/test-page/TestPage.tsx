764d20da832598a7364f1f06ccedba19683fd695
toothlessdev <toothless042@gmail.com>
2024-01-08 01:22:14 +0900

    feat: 테스트 홈페이지 구현 resolve #26

--- src/pages/test-page/TestPage.tsx
+++ src/pages/test-page/TestPage.tsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 import { Button } from "../../interfaces/forms/Button";
 import styles from "./TestPage.module.scss";
 
 export default function TestPage() {
+    const navigate = useNavigate();
+
     return (
         <div className={styles.page_wrapper}>
-            <img src="/img/test-background.svg" alt="" />
+            <img className={styles.page_bg} src="/img/test-background.svg" alt="" />
             <div className={styles.banner}>
                 <h2>2023 당신의 올해 금융점수는 ?</h2>
                 <h1>금융역량테스트</h1>
@@ -13,7 +17,7 @@ export default function TestPage() {
                     <div>나</div>
                     <div>의</div>
                     <div className={styles.img_grid}>
-                        <img src="/icons/stocodi.svg" alt="" />
+                        <img src="/icons/dollar.png" alt="" />
                     </div>
                     <div>금</div>
                     <div className={styles.img_grid}>
@@ -25,7 +29,8 @@ export default function TestPage() {
                     <div>는</div>
                 </div>
             </div>
-            <Button type="primary-stroke" width="250px" height="50px">
+
+            <Button type="primary-stroke" width="250px" height="50px" onClick={() => navigate("/test/1")}>
                 <span>테스트 시작하기</span>
             </Button>
         </div>
